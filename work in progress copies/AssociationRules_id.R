939e63a4415ae5207e7e3facb01a5a0a7dbefd69
Indy Dhillon <indy.dhillon1@gmail.com>
2020-11-22 14:33:15 -0800

    Defined rules per meeting, used all rows in data

--- work in progress copies/AssociationRules_id.R
+++ work in progress copies/AssociationRules_id.R
@@ -28,8 +28,17 @@ data$graduated = as.factor(data$graduated)
 data$income_50k = as.factor(data$income_50k)
 
 income_data = data %>% select(
-  age_group, det_ind_code, det_occ_code, major_occ_code, sex, education,
-  marital_stat, race, tax_filer_stat, citizenship, income_50k
+  age_group, 
+  det_ind_code, 
+  det_occ_code, 
+  major_occ_code, 
+  sex, 
+  education,
+  marital_stat, 
+  race, 
+  tax_filer_stat, 
+  citizenship, 
+  income_50k
   )
 grad_data = data %>% select(
   age_group, det_ind_code, det_occ_code, major_occ_code, sex,
@@ -41,31 +50,32 @@ grad_data = data %>% select(
 
 #isets = apriori(selection, parameter = list(target ="frequent", support = 0.5))
 
-rules_grad = apriori(grad_data, parameter = list(support = 0.03, confidence = 0.65), appearance = list(default="lhs",rhs="graduated=yes"))
-inspect(head(rules, n = 10))
-quality(head(rules)) #print the quality of the measures
+rules_grad = apriori(grad_data, parameter = list(support = 0.03, confidence = 0.74), appearance = list(default="lhs",rhs="graduated=yes"))
+inspect(head(rules_grad, n = 10))
+quality(head(rules_grad)) #print the quality of the measures
 
 is_max_grad = rules_grad[is.maximal(rules_grad)]
-inspect(head(sort(is_max_grad, by = "support")))
+inspect(head(sort(is_max_grad, by = "lift")))
 length(is_max_grad)
+
 
 
 rules_income = apriori(
   income_data, 
-  parameter = list(support = 0.01, confidence = 0.4), appearance = list(default="lhs",rhs="income_50k= 50000+.")
-  )
+  parameter = list(support = 0.015, confidence = 0.25), appearance = list(default="lhs",rhs="income_50k= 50000+.")
+)
 inspect(head(rules_income, n = 10))
 quality(head(rules_income)) #print the quality of the measures
 
-
 is_max = rules_income[is.maximal(rules_income)]
-inspect(head(sort(is_max, by = "support")))
+inspect((sort(is_max, by = "lift")))
 length(is_max)
 
 
 ######  Vizualization  #####
 plot(rules_grad)
+plot(rules_income)
 
 # a great plot
 plot(rules_grad, method="grouped")
-
+plot(rules_income, method="grouped")
